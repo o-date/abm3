fd0d7f0db40686cd83493aaa06d1dc4ed0079fea
Shawn Graham <shawn_graham@carleton.ca>
2018-08-25 18:32:23 -0400

    first

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages("rJava", repos='http://cran.us.r-project.org')
+install.packages("dplyr", repos='http://cran.us.r-project.org')
+install.packages("ggplot2", repos='http://cran.us.r-project.org')
+install.packages("RNetLogo", repos='http://cran.us.r-project.org')
