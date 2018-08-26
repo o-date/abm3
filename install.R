bbb39a93420f62ddcb57aecc5d1238b116a06a67
Shawn Graham <shawn_graham@carleton.ca>
2018-08-25 20:18:19 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,3 @@
-install.packages("rJava", repos='http://cran.us.r-project.org')
 install.packages("dplyr", repos='http://cran.us.r-project.org')
 install.packages("ggplot2", repos='http://cran.us.r-project.org')
 install.packages("RNetLogo", repos='http://cran.us.r-project.org')
