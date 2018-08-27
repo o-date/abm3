3f1cdcb25691f36a55c7643256c20c47f6cda46f
Shawn Graham <shawn_graham@carleton.ca>
2018-08-26 21:21:18 -0400

    Delete install.R

--- install.R
+++
@@ -1,4 +0,0 @@
-install.packages("dplyr", repos='http://cran.us.r-project.org')
-install.packages("ggplot2", repos='http://cran.us.r-project.org')
-install.packages("rJava", repos='http://cran.us.r-project.org')
-install.packages("RNetLogo", repos='http://cran.us.r-project.org')
